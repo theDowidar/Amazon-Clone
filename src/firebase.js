351de6ac56dd30e8435e07d9560bea68f1d19864
theDowidar <a7mad.dowidar@gmail.com>
2022-01-23 17:17:13 -0600

    Amazon Clone Home Page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0AVIisxRoURk81zNMvXo2BCLqzEN-o4E",
+  authDomain: "clone-f413b.firebaseapp.com",
+  projectId: "clone-f413b",
+  storageBucket: "clone-f413b.appspot.com",
+  messagingSenderId: "334437142583",
+  appId: "1:334437142583:web:5eea8a58c3fe4ecd6c2fb2",
+  measurementId: "G-6VMHJDWRLY",
+};
