3f0a0a3348181097f17fdfa76d62d8ea1e3c980a
theDowidar <a7mad.dowidar@gmail.com>
2022-01-29 15:01:26 -0600

    add front-end login

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
-    <div className="app">
-      <Header />
+      <div className="app">
 
-      <Switch>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
 
-        <Route path="/checkout">
-          <Checkout />
-        </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
 
-        <Route path="/">
-          <Home />
-        </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
 
-      </Switch>
-    </div>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -22,20 +22,23 @@ function Header() {
                 </div>
 
                 <div className="header__nav">
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Hello Guest</span>
-                        <span className="header__optionLineTwo">Sign In</span>
-                    </div>
+                    <Link to="/login">
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Hello Guest</span>
+                            <span className="header__optionLineTwo">Sign In</span>
+                        </div>
+                    </Link>
 
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Returns</span>
-                        <span className="header__optionLineTwo">& Orders</span>
-                    </div>
 
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Your</span>
-                        <span className="header__optionLineTwo">Prime</span>
-                    </div>
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Returns</span>
+                            <span className="header__optionLineTwo">& Orders</span>
+                        </div>
+
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Your</span>
+                            <span className="header__optionLineTwo">Prime</span>
+                        </div>
 
                     <Link to="/checkout">
                         <div className="header__optionBasket">
